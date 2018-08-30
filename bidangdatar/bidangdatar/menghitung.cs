bd7402343a3e02494df09021b2627b6c000a60cf
beecoder77 <beecoder77@gmail.com>
2018-08-30 12:33:23 +0700

    Operasi Bidang Datar dengan Library sendiri

---
+++ bidangdatar/bidangdatar/menghitung.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bidangdatar
+{
+    public class menghitung
+	{
+		public double persegi(double sisi)
+		{
+			return 4*sisi;
+		}
+		public double luasPersegi(double sisi)
+		{
+			return sisi * sisi;
+		}
+		public double persegipanjang(double panjang, double lebar)
+		{
+			return 2 * (panjang + lebar);
+		}
+		public double LuasPersegipanjang(double panjang, double lebar)
+		{
+			return panjang * lebar;
+		}
+		public double luasSegitiga(double alas, double tinggi)
+		{
+			return alas * tinggi / 2;
+		}
+	}
+}
---
+++ library/bidangdatarlibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bidangdatar;
+
+namespace bidangdatarlibrary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			menghitung hitung = new menghitung();
+
+			Console.WriteLine("---KELILING PERSEGI---");
+			Console.WriteLine(hitung.persegi(5));
+			Console.WriteLine("---LUAS PERSEGI---");
+			Console.WriteLine(hitung.luasPersegi(5));
+			Console.WriteLine("---KELILING PERSEGI PANJANG---");
+			Console.WriteLine(hitung.persegipanjang(10, 5));
+			Console.WriteLine("---LUAS PERSEGI PANJANG---");
+			Console.WriteLine(hitung.LuasPersegipanjang(10, 5));
+			Console.WriteLine("---LUAS SEGITIGA---");
+			Console.WriteLine(hitung.luasSegitiga(2, 2));
+			Console.ReadLine();
+		}
+	}
+}
